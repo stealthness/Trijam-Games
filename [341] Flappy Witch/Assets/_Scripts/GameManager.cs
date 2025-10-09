b6e6ea9ac8446927cce895b8944f1a8f08294439
Stealthness <stevewestinlondon@gmail.com>
2025-10-09 19:26:49 +0100

    fixed .gitignore, add basic gamemanger

---
+++ [341] Flappy Witch/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("GameManager Start");
+        }
+
+    }
+}
