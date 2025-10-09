811cfc072d6e443666445d980fce33c9a21dc36d
Stealthness <stevewestinlondon@gmail.com>
2025-10-09 19:38:34 +0100

    added playercontroller

---
+++ [341] Flappy Witch/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts
+{
+    [RequireComponent (typeof(Rigidbody2D))]
+    public class PlayerController : MonoBehaviour
+    {
+        
+        private Rigidbody2D _rigidbody2D;
+        [SerializeField] private float jumpForce;
+
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        public void OnJump()
+        {
+            
+            _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            Debug.Log("Player Jumped");
+        }
+    }
+}
