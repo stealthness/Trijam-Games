25ec1529a4dc51172736f3a6658b97cedc9220fa
Stealthness <stevewestinlondon@gmail.com>
2025-10-16 12:07:03 +0100

    Tidy background scripts

--- [341] Flappy Witch/Assets/_Scripts/Core/Background.cs
+++ [341] Flappy Witch/Assets/_Scripts/Core/Background.cs
@@ -4,23 +4,47 @@ using UnityEngine;
 namespace _Scripts.Core
 {
     /// <summary>
-    /// Controls the background scrolling effect.
+    /// Controls the background scrolling effect. Assumptions: Background sprites are arranged side by side.
+    /// The width of each background sprite is larger than twice the camera width, all are the same size.
+    /// The default scroll direction is left.
     /// </summary>
     public class Background : MonoBehaviour
     {
+        /// <summary>
+        /// The direction in which the background scrolls.
+        /// </summary>
+        [SerializeField] private Vector3 scrollDirection = Vector2.left;
+        /// <summary>
+        /// array of background sprites to be scrolled.
+        /// </summary>
         public GameObject[] backgroundSprites;
-        
-        [SerializeField] private float backgroundScrollSpeedRation = 2f;
+        /// <summary>
+        /// The ratio of the background scroll speed to the game speed.
+        /// </summary>
+        [SerializeField] private float backgroundScrollSpeedRatio = 2f;
+        /// <summary>
+        /// The width of the background sprites.
+        /// </summary>
         [SerializeField] private float backgroundWidth = 35f;
+        
+        /// <summary>
+        /// The current game scroll speed, derived from the GameManager's game speed.
+        /// </summary>
+        private float _backgroundScrollSpeed;
+        
+        
+
+        private void Start()
+        {
+            backgroundWidth = backgroundSprites[0].GetComponent<SpriteRenderer>().bounds.size.x;
+            _backgroundScrollSpeed = GameManager.Instance.gameSpeed / backgroundScrollSpeedRatio;;
+        }
 
         private void Update()
         {
-            
             foreach (var sprite in backgroundSprites)
             {
-                var backgroundScrollSpeed = GameManager.Instance.gameSpeed / backgroundScrollSpeedRation;
-                
-                sprite.transform.position += Vector3.left * (backgroundScrollSpeed * Time.deltaTime);
+                sprite.transform.position += scrollDirection * (_backgroundScrollSpeed * Time.deltaTime);
                 if (sprite.transform.position.x <= -backgroundWidth)
                 {
                     sprite.transform.position += Vector3.right * (2 * backgroundWidth);
