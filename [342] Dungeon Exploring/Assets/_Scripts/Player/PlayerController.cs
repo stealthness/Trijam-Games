af453b03396d558561efe77283db36736a4ba346
Stealthness <stevewestinlondon@gmail.com>
2025-10-18 00:30:36 +0100

    Added player controller

---
+++ [342] Dungeon Exploring/Assets/_Scripts/Player/PlayerController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _Scripts.Player
+{
+    
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(SpriteRenderer))]
+    //[RequireComponent(typeof(PlayerAnimator))]
+    public class PlayerController : MonoBehaviour
+    {
+        private Rigidbody2D _rb;
+        private SpriteRenderer _sr;
+        //private PlayerAnimator _playerAnimator;
+        
+        [SerializeField] private float speed = 5f;
+        [SerializeField] private float jumpForce = 10f;
+        [SerializeField] private Vector2 direction;
+        private const float Tol = 0.01f;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _rb.gravityScale = 0;
+            _rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            //_playerAnimator = GetComponent<PlayerAnimator>();
+            _sr = GetComponent<SpriteRenderer>();
+        }
+        
+
+        /// <summary>
+        /// OnMove is called when the player moves
+        /// </summary>
+        public void OnMove(InputValue value)
+        {
+
+                direction = value.Get<Vector2>();
+                ChangeDirection();
+                //_playerAnimator.PlayAnimation(PlayerAnimation.Moving);
+
+        }
+
+        private void LateUpdate()
+        {
+            _rb.linearVelocity = speed * new Vector2(direction.x, direction.y);
+        }
+
+        /// <summary>
+        ///  Change the direction of the sprite as per the direction of the player
+        /// </summary>
+        private void ChangeDirection()
+        {
+            _sr.flipX = direction.x switch
+            {
+                < -Tol => true,
+                > Tol => false,
+                //else => _sr.flipX // If the direction is zero, keep the current flip state
+                _ => _sr.flipX
+            };
+        }
+    }
+}
