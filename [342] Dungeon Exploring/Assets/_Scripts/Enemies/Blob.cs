39655179d7eaf06107f1d6bf8dc0fb7cfd554f84
Stealthness <stevewestinlondon@gmail.com>
2025-10-21 10:32:30 +0100

    tidying up Blob.cs, added OnDrawGizmo to aid debugging

--- [342] Dungeon Exploring/Assets/_Scripts/Enemies/Blob.cs
+++ [342] Dungeon Exploring/Assets/_Scripts/Enemies/Blob.cs
@@ -7,8 +7,19 @@ namespace _Scripts.Enemies
     {
         public GameObject blobIdle;
         public GameObject blobExplode;
+        public GameObject blobAttack;
+        public GameObject rightCloud;
+        public GameObject leftCloud;
 
         public AudioSource audioSource;
+        
+        private GameObject _activeBlobState;
+        
+         private void Start()
+         {
+             SetBlobState(BlobState.Idle);
+             InvokeRepeating(nameof(BlobAttack), 5f, 5f);
+         }       
 
         private void OnCollisionEnter2D(Collision2D other)
         {
@@ -32,20 +43,38 @@ namespace _Scripts.Enemies
             }
         }
 
+        private void OnDrawGizmos()
+        {
+            var box = GetComponent<BoxCollider2D>();
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(transform.position + (Vector3)box.offset, box.size);
+        }
+
+
+
         private void ExplodeBlob()
         {
-            blobExplode.SetActive(true);
-            blobIdle.SetActive(false);
+            SetBlobState(BlobState.Exploding);
             audioSource.Play();
             var animationLength = GetCurrentAnimationClipLength();
             Debug.Log("Animation Length: " + animationLength);
             Destroy(gameObject, animationLength);
         }
 
+        
+        private void BlobAttack()
+        {
+            SetBlobState(BlobState.Attacking);
+            var animationLength = GetCurrentAnimationClipLength();
+            Debug.Log("Attack Animation Length: " + animationLength);
+            Invoke(nameof(ResetToIdle), animationLength);
+            leftCloud.SetActive(true);
+            rightCloud.SetActive(true);
+        }
 
         private float GetCurrentAnimationClipLength(int layer = 0)
         {
-            var clips = blobExplode.GetComponent<Animator>().GetCurrentAnimatorClipInfo(layer);
+            var clips = _activeBlobState.GetComponent<Animator>().GetCurrentAnimatorClipInfo(layer);
             if (clips != null && clips.Length > 0)
             {
                 var clip = clips[0].clip;
@@ -54,5 +83,38 @@ namespace _Scripts.Enemies
             }
             return 0f;
         }
+        
+        private void ResetToIdle()
+        {
+            SetBlobState(BlobState.Idle);
+        }
+        
+        
+        private void SetBlobState(BlobState state)
+        {
+            blobIdle.SetActive(state == BlobState.Idle);
+            blobExplode.SetActive(state == BlobState.Exploding);
+            blobAttack.SetActive(state == BlobState.Attacking);
+
+            if (state != BlobState.Attacking){
+                leftCloud.SetActive(false);
+                rightCloud.SetActive(false);
+            }
+            
+            _activeBlobState = state switch
+            {
+                BlobState.Idle => blobIdle,
+                BlobState.Exploding => blobExplode,
+                BlobState.Attacking => blobAttack,
+                _ => blobIdle
+            };
+        }
+    }
+
+    internal enum BlobState
+    {
+        Idle,
+        Exploding,
+        Attacking
     }
 }
---
+++ [342] Dungeon Exploring/Assets/_Scripts/Enemies/BlobGasCloud.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using UnityEngine;
+
+namespace _Scripts.Enemies
+{
+    public class BlobGasCloud : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            Debug.Log("BlobGasCloud::OnEnable");
+        }
+    }
+}
