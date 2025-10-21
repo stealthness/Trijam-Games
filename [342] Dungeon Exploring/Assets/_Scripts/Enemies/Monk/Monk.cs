ea0b4ab118ec2ebf834b758eec442b0029f5cfca
Stealthness <stevewestinlondon@gmail.com>
2025-10-21 15:06:18 +0100

    reordering some directories and namespaces

---
+++ [342] Dungeon Exploring/Assets/_Scripts/Enemies/Monk/Monk.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace _Scripts.Enemies.Monk
+{
+    public class Monk : WaypointFollower
+    {
+
+        private new void Start()
+        {
+            base.Start();
+            speed = 3f;
+            waitTime = 0f;
+        }
+        
+        protected new IEnumerator MoveToPoint()
+        {
+
+            while (true)
+            {
+                
+                var targetPoint = waypoints[pointIndex];
+                while (Vector2.Distance(enemyObject.transform.position, targetPoint.position) > 0.1f)
+                {
+                    
+                    enemyObject.transform.position = Vector3.MoveTowards(enemyObject.transform.position, 
+                        targetPoint.position, speed * Time.deltaTime);
+                    yield return null;
+                }
+
+                pointIndex = (pointIndex + 1) % waypoints.Length;
+                CheckDirection(targetPoint.position.x);
+                yield return new WaitForSeconds(waitTime);
+            }
+        }
+
+        private void CheckDirection(float dir)
+        {
+            enemyObject.transform.localScale = dir > transform.position.x ? new Vector3(1, 1, 1) : new Vector3(-1, 1, 1);
+        }
+    }
+}
