cb79eb52990fd490211a504e7e8c8c0aeed1a7ab
Stealthness <stevewestinlondon@gmail.com>
2025-10-23 01:28:08 +0100

    working on snake pogo sticks

--- [Mini Jam 196] Snakes on Pogo Sticks/Assets/_Scripts/Player/PlayerController.cs
+++ [Mini Jam 196] Snakes on Pogo Sticks/Assets/_Scripts/Player/PlayerController.cs
@@ -1,12 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace _Scripts.Player
 {
     public class PlayerController : MonoBehaviour
     {
-        public void OnMove(Vector3 direction)
+        [SerializeField] private Vector2 direction = Vector2.up;
+        [SerializeField] private float speed = 5f;
+
+        public void OnMove(InputValue value)
         {
-            transform.Translate(direction * Time.deltaTime * 5f);
+            Vector2 inputDirection = value.Get<Vector2>();
+            if (inputDirection.x == 0)
+            {
+                direction = Vector2.zero;
+            }
+            else if (inputDirection.x < 0)
+            {
+                direction = Vector2.left;
+            }
+            else
+            {
+                direction = Vector2.right;
+            }
+            
+            
+        }
+
+        private void LateUpdate()
+        {
+            transform.Translate(direction * (speed * Time.deltaTime), Space.World);
         }
     }
 }
--- [Mini Jam 196] Snakes on Pogo Sticks/Assets/_Scripts/Snakes/SnakeConroller.cs
+++ [Mini Jam 196] Snakes on Pogo Sticks/Assets/_Scripts/Snakes/SnakeConroller.cs
@@ -1,9 +1,35 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace _Scripts.Snakes
 {
     public class SnakeController : MonoBehaviour
     {
         
+        [SerializeField] private Vector2 direction = Vector2.up;
+        [SerializeField] private float speed = 3f;
+
+        private void Update()
+        {
+            if (transform.position.y > 7f)
+            {
+                var randomX = UnityEngine.Random.Range(-1f, 1f);
+                var downY = -1f;
+                direction = new Vector2(randomX, downY).normalized;
+            }
+            
+            if (transform.position.y < 0.2f)
+            {
+                var randomX = UnityEngine.Random.Range(-1f, 1f);
+                var upY = 1f;
+                direction = new Vector2(randomX, upY).normalized;
+            }
+        }
+
+
+        private void LateUpdate()
+        {
+            transform.Translate(direction * (speed * Time.deltaTime), Space.World);
+        }
     }
 }
