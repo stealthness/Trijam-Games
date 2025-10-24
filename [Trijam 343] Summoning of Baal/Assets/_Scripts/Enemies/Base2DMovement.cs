2cba724aac83967053608f40b5f6cc08b61a581b
Stealthness <stevewestinlondon@gmail.com>
2025-10-24 03:04:33 +0100

    Base Movement script added

---
+++ [Trijam 343] Summoning of Baal/Assets/_Scripts/Enemies/Base2DMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Enemies
+{
+    public class Base2DMovement : MonoBehaviour
+    {
+        private Vector2 _direction = Vector2.up;
+        [SerializeField] private float moveSpeed = 1f;
+
+
+        private void LateUpdate()
+        {
+            transform.Translate(_direction * (moveSpeed * Time.deltaTime), Space.World);
+        }
+    }
+}
