d8aa02eda4de5bd268d569020066599f3a5e09ca
Stealthness <stevewestinlondon@gmail.com>
2025-10-24 12:19:35 +0100

    added game manager

--- [Trijam 343] Summoning of Baal/Assets/_Scripts/Managers/GameManager.cs
+++ [Trijam 343] Summoning of Baal/Assets/_Scripts/Managers/GameManager.cs
@@ -1,9 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace a
 {
     public class GameManager : MonoBehaviour
     {
-        
+        public static GameManager Instance;
+
+        private void Awake()
+        {
+            if(!Instance)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+
+        private void Start()
+        {
+            Debug.Log("Game Manager Started");
+        }
     }
 }
