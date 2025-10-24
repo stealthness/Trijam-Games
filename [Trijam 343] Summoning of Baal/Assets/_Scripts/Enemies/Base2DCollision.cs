e2f550bf98a6c5ad27e4a68b12d177c605a4d0ad
Stealthness <stevewestinlondon@gmail.com>
2025-10-24 09:33:04 +0100

    basic collision done player Health added

---
+++ [Trijam 343] Summoning of Baal/Assets/_Scripts/Enemies/Base2DCollision.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Enemies
+{
+    public class Base2DCollision : MonoBehaviour
+    {
+        
+    }
+}
---
+++ [Trijam 343] Summoning of Baal/Assets/_Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts.Player
+{
+    public class PlayerHealth : MonoBehaviour
+    {
+        [SerializeField] private int health = 100;
+        [SerializeField] private int maxHealth = 100;
+
+
+        private void Start()
+        {
+            health = maxHealth;
+        }
+
+
+        public void TakeDamage(int damageAmount)
+        {
+            
+            health -= damageAmount;
+            Debug.Log("Player took damage");
+            
+            if (damageAmount <= 0)
+            {
+                health = 0;
+                PlayerHasDied();
+            }
+        }
+
+        private void PlayerHasDied()
+        {
+            Time.timeScale = 0;
+            Debug.Log("Player has died");
+        }
+    }
+}
