24b2b85f177cf2c028a72702c6a4629e3169a7df
Stealthness <stevewestinlondon@gmail.com>
2025-10-25 10:32:29 +0100

    minor

--- [342] The summoning/Assets/_Scripts/Enemy/Enemy.cs
+++ [342] The summoning/Assets/_Scripts/Enemy/Enemy.cs
@@ -1,60 +1,92 @@
 ﻿using System;
+using UnityEditor;
 using UnityEngine;
 
 namespace _Scripts.Enemy
 {
     public class Enemy : MonoBehaviour
     {
+        private Animator _animator;
+        
         private Vector2 _direction;
         [SerializeField] private float enemySpeed = 2f;
+        [SerializeField] private SmallEnemyState state;
 
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            state = SmallEnemyState.Moving;
+        }
 
         private void Update()
         {
-            if (transform.position.x > 0)
-            {
-                _direction = Vector2.left;
-            }
-            else
-            {
-                _direction = Vector2.right;
-            }
-            
+            _direction = transform.position.x > 0 ? Vector2.left : Vector2.right;
             checkCollision();
         }
 
         private void checkCollision()
         {
             var box = GetComponent<BoxCollider2D>();
-            var hit = Physics2D.OverlapBox(box.bounds.center, box.bounds.size, 0);
-            if (!hit)
+            var hits = Physics2D.OverlapBoxAll(box.bounds.center, box.bounds.size, 0);
+            if (hits.Length == 0)
             {
                 return;
             }
-            
-            
-            
-            if (hit.CompareTag("Monk"))
-            {
-                Debug.Log("Enemy: Enemy hit Monk!");
-                Destroy(gameObject);
-            }
 
-            if (hit.CompareTag("Player"))
+            foreach (var hit in hits)
             {
-                Debug.Log("Enemy: Enemy hit Player!");
+                if (hit.CompareTag("Monk"))
+                {
+                    Debug.Log("Enemy: Enemy hit Monk!");
+                    Destroy(gameObject);
+                }
+
+                if (hit.CompareTag("Player"))
+                {
+                    Debug.Log("Enemy: Enemy hit Player!");
+                }
+
+                if (hit.CompareTag("Weapon"))
+                {
+                    Debug.Log("Enemy: Hit by weapon!");
+                    Explode();
+                }
             }
-            
-            if (hit.CompareTag("Weapon"))
-            {
-                Debug.Log("Enemy: Hit by weapon!");
-            }
+        }
+
+        private void Explode()
+        {
+            state = SmallEnemyState.Dying;
+            _animator.SetTrigger("Explode");
         }
 
 
         private void LateUpdate()
         {
-            transform.Translate(_direction * (Time.deltaTime * enemySpeed));
+            switch (state)
+            {
+                case SmallEnemyState.Dying:
+                    break;
+                
+                case SmallEnemyState.Moving:
+                default:
+                    transform.Translate(_direction * (Time.deltaTime * enemySpeed));
+                    break;
+            }
         }
+    }
+
+
+    enum SmallEnemyState
+    {
+        Idle,
+        Moving,
+        Charging,
+        Attacking,
+        Dying
     }
 }
