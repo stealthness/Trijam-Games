d8de9f0c86081cad18eeb30498f640777208b8fc
Stealthness <stevewestinlondon@gmail.com>
2025-10-27 12:42:12 +0000

    Tidying up removing unused code

--- [Mini Jam 196] Snakes on Pogo Sticks/Assets/_Scripts/Managers/GameManager.cs
+++ [Mini Jam 196] Snakes on Pogo Sticks/Assets/_Scripts/Managers/GameManager.cs
@@ -4,13 +4,26 @@ using UnityEngine.SceneManagement;
 
 namespace _Scripts.Managers
 {
+    
+    /// <summary>
+    /// The GameManager class is responsible for managing the overall game state,
+    /// </summary>
     public class GameManager : MonoBehaviour
     {
+        /// <summary>
+        /// A singleton instance of the GameManager.
+        /// </summary>
         public static GameManager Instance;
-        
-        public GameObject EmptyBoard;
+        /// <summary>
+        /// An empty board GameObject to display when the game is not active.
+        /// </summary>
+        public GameObject emptyBoard;
+        /// <summary>
+        /// The current turn type in the game.
+        /// </summary>
         public TurnType gameTurn;
         
+        [Tooltip("Delay in seconds before the enemy takes its turn.")]
         [SerializeField] private float enemyTurnDelay = 2f;
 
         private void Awake()
@@ -30,46 +43,42 @@ namespace _Scripts.Managers
             Debug.Log("Game Manager Started");
             Time.timeScale = 0;
             gameTurn = TurnType.NoTurn;
-            EmptyBoard.SetActive(true);
+            emptyBoard.SetActive(true);
             MenuManager.Instance.ShowMainMenu();
-            
         }
 
+        /// <summary>
+        /// Starts the game by setting the time scale to normal and initializing the player's turn.
+        /// </summary>
         public void StartGame()
         {
             Debug.Log("Game Started");
             Time.timeScale = 1;
             gameTurn = TurnType.PlayerTurn;
-            EmptyBoard.SetActive(false);
+            emptyBoard.SetActive(false);
             MenuManager.Instance.ShowTurnMenu(TurnType.PlayerTurn);
         }
 
-        public void NextTurn()
-        {
-            if (gameTurn == TurnType.PlayerTurn)
-            {
-                gameTurn = TurnType.EnemyTurn;
-                StartEnemyTurn();
-            }
-            else if (gameTurn == TurnType.EnemyTurn)
-            {
-                gameTurn = TurnType.PlayerTurn;
-            }
-            MenuManager.Instance.ShowTurnMenu(gameTurn);
-        }
-
+        /// <summary>
+        /// Starts the enemy's turn after a specified delay.
+        /// </summary>
         private void StartEnemyTurn()
         {
             Invoke(nameof(PlayerTurn), enemyTurnDelay);
         }
 
+        /// <summary>
+        /// Starts the player's turn and updates the UI accordingly.
+        /// </summary>
         public void PlayerTurn()
         {
             gameTurn = TurnType.PlayerTurn;
             MenuManager.Instance.ShowTurnMenu(gameTurn);
         }
         
-        
+        /// <summary>
+        /// Starts the enemy's turn and updates the UI accordingly.
+        /// </summary>
         public void EnemyTurn()
         {
             gameTurn = TurnType.EnemyTurn;
@@ -85,7 +94,6 @@ namespace _Scripts.Managers
 
         public void NextWave()
         {
-            Debug.Log("Wave Complete");
             WaveManager.Instance.StartNextWave();
         }
     }
