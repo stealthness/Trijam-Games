1290a78359edd22a2612c084f56bd74903347236
Stealthness <stevewestinlondon@gmail.com>
2025-10-27 23:13:03 +0000

    moved method to utility class

--- [Mini Jam 196] Snakes on Pogo Sticks/Assets/_Scripts/Core/GameObjectUtils.cs
+++ [Mini Jam 196] Snakes on Pogo Sticks/Assets/_Scripts/Core/GameObjectUtils.cs
@@ -8,9 +8,9 @@ namespace _Scripts.Core
         public static int[] ShuffleGameObjectsIndices<T>(T[] array)
         {
             List<int> indices = new List<int>(array.Length);
-            for (int i = 0; i < array.Length; i++) indices.Add(i);
+            for (var i = 0; i < array.Length; i++) indices.Add(i);
 
-            for (int i = indices.Count - 1; i > 0; i--)
+            for (var i = indices.Count - 1; i > 0; i--)
             {
                 var j = Random.Range(0, i + 1);
                 (indices[i], indices[j]) = (indices[j], indices[i]);
