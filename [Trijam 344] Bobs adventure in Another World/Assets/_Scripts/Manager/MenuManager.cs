a24b77fb705c57c21acaad2624ecae89d51ff8a3
Stealthness <stevewestinlondon@gmail.com>
2025-11-01 21:24:31 +0000

    adding function to menuManager

--- [Trijam 344] Bobs adventure in Another World/Assets/_Scripts/Manager/MenuManager.cs
+++ [Trijam 344] Bobs adventure in Another World/Assets/_Scripts/Manager/MenuManager.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace _Scripts.Manager
 {
     public class MenuManager : MonoBehaviour
     {
         public GameObject startMenuPanel;
+        public GameObject infoPanel;
+        public GameObject diedMenuPanel;
+
+
+        private void Start()
+        {
+            startMenuPanel.SetActive(true);
+            diedMenuPanel.SetActive(false);
+        }
         
         public void OnStartButtonPressed()
         {
             GameManager.Instance.StartGame();
             startMenuPanel.SetActive(false);
+        }
+
+        public void OnStartOkButtonClicked()
+        {
+            
+            infoPanel.SetActive(false);
+        }
+        
+
+        public void OnDiedOkButtonPressed()
+        {
+            SceneManager.LoadScene(0);
         }
     }
 }
