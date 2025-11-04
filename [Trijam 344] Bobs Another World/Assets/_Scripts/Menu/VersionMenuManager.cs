61a54359c6de53fe986cf39d6ae7c4a5810e23e3
Stealthness <stevewestinlondon@gmail.com>
2025-11-04 14:28:40 +0000

    moved classes created VersionMenuManagers

---
+++ [Trijam 344] Bobs Another World/Assets/_Scripts/Menu/VersionMenuManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Scripts.Menu
+{
+    /// <summary>
+    /// This class manages the version selection menu.
+    /// It allows players to choose between different game versions and loads the corresponding scenes.
+    /// </summary>
+    public class VersionMenuManager : MonoBehaviour
+    {
+        
+        
+        
+        
+        /// <summary>
+        /// This function is called when a game version is selected from the menu.
+        /// It loads the corresponding scene based on the selected version.
+        /// </summary>
+        /// <param name="selection"></param>
+        public void OnSelectionClick(GameVersion selection)
+        {
+            var selectedVersion = selection switch
+            {
+                GameVersion.TrijamOriginal => "TrijamOriginal",
+                GameVersion.TrijamRemastered => "TrijamRemastered",
+                GameVersion.UntitledOriginal => "UntitledOriginal",
+                GameVersion.UntitledRemastered => "UntitledRemastered",
+                _ => "TrijamOriginal"
+            };
+            SceneManager.LoadScene(selectedVersion);
+        }
+    }
+
+    public enum GameVersion
+    {
+        TrijamOriginal,
+        TrijamRemastered,
+        UntitledOriginal,
+        UntitledRemastered
+    }
+}
