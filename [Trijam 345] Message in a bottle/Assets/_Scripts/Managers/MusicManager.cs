05f5e2a353e58ec1304c80c13e5e83170afe4579
Stealthness <stevewestinlondon@gmail.com>
2025-11-10 09:28:47 +0000

    added Music manager to toggle music on off

---
+++ [Trijam 345] Message in a bottle/Assets/_Scripts/Managers/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Managers
+{
+    public class MusicManager : MonoBehaviour
+    {
+        public static MusicManager Instance;
+        public AudioSource musicSource;
+        
+        private void Awake()
+        {
+            if(!Instance || Instance != this)
+            {
+                Destroy(Instance);
+            }
+            Instance = this;
+            if (!musicSource)
+            {
+                Debug.LogWarning("Music Source is not assigned in the inspector.");
+            }
+        }
+
+
+        public void ToggleMusic(bool toggle)
+        {
+            Debug.Log("MusicManager ToggleMusic: " + toggle);
+            if (toggle)
+            {
+                musicSource.Play();
+            }
+            else
+            {
+                musicSource.Pause();
+            }
+        }
+
+    }
+}
