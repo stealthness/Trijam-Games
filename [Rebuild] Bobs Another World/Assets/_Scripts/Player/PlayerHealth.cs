f76f9d0ba3a9db075fa5cde33707f12bbf82cd4a
Stealthness <stevewestinlondon@gmail.com>
2025-11-15 10:17:05 +0000

    added player Health

---
+++ [Rebuild] Bobs Another World/Assets/_Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace _Scripts.Player
+{
+    
+    [RequireComponent(typeof(PlayerController))]
+    public class PlayerHealth : MonoBehaviour
+    {
+        private PlayerController _pc;
+        
+        [SerializeField] private int maxHealth = 100;
+        [SerializeField] private int currentHealth;
+        
+        
+        private void Awake()
+        {
+            _pc = GetComponent<PlayerController>();
+            currentHealth = maxHealth;
+        }
+
+        public void TakeDamage(int amount)
+        {
+            currentHealth -= amount;
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                Debug.Log("Player Dead");
+            }
+            Debug.Log("Player Take Damage -> current health" + currentHealth);
+        }
+    }
+}
