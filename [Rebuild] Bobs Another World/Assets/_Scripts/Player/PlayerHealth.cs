17ec7bd7739429a6e70b54c0a9ccdc5cc04188f7
Stealthness <stevewestinlondon@gmail.com>
2025-11-16 17:37:23 +0000

    added doc  and isInvincible attribute scene changes

--- [Rebuild] Bobs Another World/Assets/_Scripts/Player/PlayerHealth.cs
+++ [Rebuild] Bobs Another World/Assets/_Scripts/Player/PlayerHealth.cs
@@ -11,6 +11,7 @@ namespace _Scripts.Player
         
         [SerializeField] private int maxHealth = 100;
         [SerializeField] private int currentHealth;
+        [SerializeField] private bool isInvincible = false;
         
         
         private void Awake()
@@ -19,8 +20,18 @@ namespace _Scripts.Player
             currentHealth = maxHealth;
         }
 
+        /// <summary>
+        /// The amount of damage to apply to the player.
+        /// </summary>
+        /// <param name="amount">the amount of damage to taken</param>
         public void TakeDamage(int amount)
         {
+            // If the player is invincible, do not apply damage.
+            if (isInvincible)
+            {
+                return;
+            }
+            
             currentHealth -= amount;
             if (currentHealth <= 0)
             {
@@ -30,9 +41,14 @@ namespace _Scripts.Player
             Debug.Log("Player Take Damage -> current health" + currentHealth);
         }
 
+        /// <summary>
+        /// Returns if the player is in a damageable state.
+        /// ie if the player has been shield then they would be invincible and not damageable.
+        /// </summary>
+        /// <returns>true if the player can be damage, false otherwise</returns>
         public bool IsDamageable()
         {
-            return true;
+            return !isInvincible;
         }
     }
 }
