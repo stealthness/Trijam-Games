ae4d80c08bd03c051e1f46c98ff0f7c63348414b
Stealthness <stevewestinlondon@gmail.com>
2025-11-18 21:56:15 +0000

    added a script to load the first scene

--- [Rebuild] Bobs Another World/Assets/_Scripts/Managers/GameManager.cs
+++ [Rebuild] Bobs Another World/Assets/_Scripts/Managers/GameManager.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace _Scripts.Managers
 {
     public class GameManager : MonoBehaviour
     {
+        [SerializeField] private string startSceneToLoad = "RocketScene";
         public static GameManager Instance { get; private set; }
 
         private void Awake()
@@ -14,11 +16,41 @@ namespace _Scripts.Managers
             }
 
             Instance = this;
+            LoadScene();
         }
 
         public void Start()
         {
             Debug.Log("Game Start");
+        }
+        
+        
+        /// <summary>
+        /// Loads the specified scene additively if it is not already loaded.
+        /// </summary>
+        private void LoadScene()
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            // Check if the scene is already loaded
+            for (var i = 0; i < SceneManager.sceneCount; i++)
+            {
+                scene = SceneManager.GetSceneAt(i);
+                if (scene.name == startSceneToLoad)
+                {
+                    Debug.Log($"{startSceneToLoad} already loaded.");
+                    return;
+                }
+            }
+            
+            Debug.Log($"Search for {startSceneToLoad}");
+            if (scene.name != startSceneToLoad)
+            {
+                SceneManager.LoadSceneAsync(startSceneToLoad, LoadSceneMode.Additive);
+            }
+            else
+            {
+                Debug.Log($"Scene {startSceneToLoad} not found among game scenes");
+            }
         }
     }
 }
---
+++ [Rebuild] Bobs Another World/Assets/_Scripts/SceneManagementScripts/GameSceneManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Scripts.SceneManagementScripts
+{
+    public class GameSceneManager : MonoBehaviour
+    {
+        public String firstSceneToLoadName;
+
+        private Scene firstSceneToLoad;
+
+        private void Awake()
+        {
+            var async = SceneManager.LoadSceneAsync(firstSceneToLoadName, LoadSceneMode.Additive);
+            if (async != null) async.completed += OnFirstSceneLoaded;
+        }
+
+        private void OnFirstSceneLoaded(AsyncOperation obj)
+        {
+            firstSceneToLoad = SceneManager.GetSceneByName(firstSceneToLoadName);
+            if (firstSceneToLoad.IsValid())
+            {
+                Debug.Log($"First scene {firstSceneToLoadName} loaded successfully.");
+            }
+            else
+            {
+                Debug.LogError($"Failed to load the first scene: {firstSceneToLoadName}");
+            }
+        }
+
+        private void Start()
+        {
+            Debug.Log("GameSceneManager started.");
+            
+            
+        }
+    }
+}
