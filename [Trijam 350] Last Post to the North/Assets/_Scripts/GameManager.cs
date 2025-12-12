b1f38de951c463a9bee22d550ea52b4ab22a48dc
Stealthness <stevewestinlondon@gmail.com>
2025-12-12 15:33:06 +0000

    start trijam 350

---
+++ [Trijam 350] Last Post to the North/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        void Start()
+        {
+            Debug.Log("Game Manager Started");
+        }
+
+
+    }
+}
