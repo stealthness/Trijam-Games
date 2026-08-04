758f50e03fe2138ce49fd8f98093d1bfdab66f70
Stealthness <stevewestinlondon@gmail.com>
2026-08-04 19:09:42 +0100

    1 bit bob player controller

---
+++ [1 bit 13] Bobs Dungeon Delve/Bobs Dungeon Delve/Assets/_GameRoot/_Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿namespace _GameRoot._Scripts
+{
+    public class GameManager
+    {
+        private static GameManager _instance;
+        public static GameManager Instance => _instance ??= new GameManager();
+
+        public void RestartGame()
+        {
+            // Implement game restart logic here
+        }
+    }
+}
